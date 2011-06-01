54f68115644f1d32d3a512529d5895514626ebd2
phntien <phntien@30cbe828-170f-2830-30ed-452c99fa32f5>
2011-06-01 16:08:47 +0000

    Cơ bản thể hiện được giao diện: Chú ý: 1.> Giao diện giống với cái bài tham khảo: Sẽ sữ lý sao. Bắt buộc không được giống với bài tham khảo 2.> Tên biến, phương thức giống bài tham thảo để còn biết đường mà lần ra chổ nào. Sao đó có thể refactor lại.
    Mấy cái này là Tiến dựa vào bài tham khảo để làm ra. Nếu có chổ nào không hiểu thì nói Tiến.

---
+++ Source/ProjectHeartGame/src/Data/MainGame.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+/**
+ *
+ * @author TienPhan
+ */
+public class MainGame {
+
+   /**
+    * @param args the command line arguments
+    */
+   public static void main(String[] args) {
+      HeartManager client1 = new HeartManager();
+
+      //client1.connectServer("Tien",2300);
+      //client1.sartPlay();
+      //HeartManager client2 = new HeartManager();
+      //client2.connectServer("Toan",2400);
+      //client2.sartPlay();
+      //HeartManager client3 = new HeartManager();
+      //client3.connectServer("Thuan",2500);
+      //client3.sartPlay();
+      //HeartManager client4 = new HeartManager();
+      //client4.connectServer("NoName",2600);
+      //client4.sartPlay();
+   }
+}
--- Source/ProjectHeartGame/src/Data/Player.java
+++ Source/ProjectHeartGame/src/Data/Player.java
@@ -6,11 +6,12 @@ package Data;
 
 import java.util.ArrayList;
 import java.util.Random;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
 /**
  *
- * @author TienPhan: sữ lý người choi
+ * @author TienPhan
  */
 public class Player extends DTOConstantPlayer {
 
@@ -29,12 +30,15 @@ public class Player extends DTOConstantPlayer {
 
    /*
     * Ham khoi tao mot nguoi choi
+    * namePlayer: Tên người chơi
+    * typePlayer: Loại người chơi
+    * positionPlayer: Vị trí người chơi
     */
    public Player(String namePlayer, int typePlayer, int positionPlayer) {
       //Xác định người chơi.
       this.name = namePlayer;
       this.type = typePlayer;
-      this.position = positionPlayer;
+      position = positionPlayer;
       //Xác định bộ bài
       listCard = new ArrayList<Card>(SOQUANBAI);
       scoreCard = new ArrayList<Card>();
@@ -52,5 +56,71 @@ public class Player extends DTOConstantPlayer {
          randomIndex[i] = randomIndex[j];
          randomIndex[j] = tmp;
       }
+   }
+
+   /*
+    * Lấy tên người chơi
+    */
+   public String getName() {
+      return name;
+   }
+
+   /*
+    * Gán nhãn lá bài mà người chơi đã chơi
+    */
+   public void setPlayCardLabel(JLabel[] playCardLabel) {
+      this.playCardLabel = playCardLabel;
+   }
+
+   /*
+    * Gán danh sách lá bài trong tay người chơi
+    */
+   public void setListCardLabel(JLabel[] listCardLabel) {
+      this.listCardLabel = listCardLabel;
+   }
+
+   /*
+    *setNextPlayer:set nguoi choi tiep theo
+    * Người chơi 1 -> ng2 -> ng3->ng4
+    */
+   public void setNextPlayer(Player nextPlayer) {
+      this.nextPlayer = nextPlayer;
+   }
+
+   /*
+    * Kiểm tra con có phải người chơi hay không
+    */
+   public boolean isHuman() {
+      return (this.type == IS_HUMAN);
+   }
+   /*
+    * Xétloại người chơi
+    */
+
+   public void setType(int type) {
+      this.type = type;
+   }
+
+   /*
+    * Hiển thị lá bài lên JLabel của người chơi
+    * show = true: hiển thị thấy hình
+    * show = false: hiển thị thấy mặt sắp 
+    */
+   public void showListCard(boolean show) {
+      int i;
+      for (i = 0; i < listCard.size(); i++) {
+         Card c = listCard.get(i);
+         if (c != null) {
+            if (show) {
+               this.listCardLabel[i].setIcon(new ImageIcon(c.creatIconFile()));
+            } else {
+
+               this.listCardLabel[i].setIcon(new ImageIcon(Card.BACK_PICTURE));
+            }
+            this.listCardLabel[i].setVisible(true);
+         }
+      }
+      // this.listCardLabel[Player.SOQUANBAI].setIcon(new ImageIcon(Card.BACK_PICTURE));
+      this.listCardLabel[Player.SOQUANBAI].setVisible(false);
    }
 }
